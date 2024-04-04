ae10c7cae2ad859736f2f28bc25624463474b34e
Fran_RF4 <fran.rf4@gmail.com>
2024-04-04 12:59:02 +0200

    inicio X

---
+++ projects/cajaFollowersX/src/App.jsx
@@ -0,0 +1,17 @@
+export function App () {
+    return(
+        <article style={{display:'flex',alignItems:'center',color:'#fff'}}>
+            <header>
+                <img alt="El avatar de Twitch" src="https://unavatar.io/Real%20Betis%20Balompie" />
+                <div><strong>Twitch</strong></div>
+                <span>@Twitch.Tv</span>
+            </header>
+
+            <aside>
+                <button> 
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
